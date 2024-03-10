48579fb5323bd9c2c918337df4780c711a0cfaa6
dazzel3 <aakk9350@naver.com>
2024-03-10 14:23:05 +0900

    refactor: use axios get function

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import VideoCard from '../components/VideoCard';
@@ -12,9 +13,9 @@ export default function Videos() {
   } = useQuery({
     queryKey: ['videos', keyword],
     queryFn: async () =>
-      fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-        .then((res) => res.json())
-        .then((data) => data.items),
+      axios
+        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => res.data.items),
   });
 
   return (
