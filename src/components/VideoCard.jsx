a0fc98bb89ad791aaf59beae4360d654b767e413
dazzel3 <aakk9350@naver.com>
2024-03-10 23:44:10 +0900

    feat: create VideoCard using data

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { formatAgo } from '../util/date';
 
 export default function VideoCard({ video }) {
-  return <div>{video.snippet.title}</div>;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li className='flex flex-col gap-1'>
+      <img
+        className='w-full rounded-lg'
+        src={thumbnails.high.url}
+        alt={title}
+      />
+      <p className='font-bold text-gray-800 text-sm/5 line-clamp-2 my-1'>
+        {title}
+      </p>
+      <p className='font-medium text-gray-500 text-xs flex flex-col gap-0.5'>
+        {channelTitle}
+        <span>{formatAgo(publishedAt, 'ko')}</span>
+      </p>
+    </li>
+  );
 }
