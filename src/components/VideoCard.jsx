d9e0ae924d6f59b28de872faa2ae5b04b7b569b8
dazzel3 <aakk9350@naver.com>
2024-03-13 10:57:28 +0900

    feat: navigate to VideoDetail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideoCard({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/videos/watch/${video.id}`);
+  };
+
   return (
-    <li className='flex flex-col gap-1'>
+    <li className='flex flex-col gap-1' onClick={handleClick}>
       <img
         className='w-full rounded-lg'
         src={thumbnails.high.url}
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function VideoDetail() {
-  return <div>VideoDetail</div>;
+  const { videoId } = useParams();
+
+  return <div>{videoId}</div>;
 }
